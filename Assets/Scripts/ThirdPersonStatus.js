5382172b00e6f3d53e319f0881f25d2338f8b41f
PoohTheBear <lylius17@gmail.com>
2011-11-29 00:38:42 +0200

    Camera Bug Fix, Meskos platforma

--- Assets/Scripts/ThirdPersonStatus.js
+++ Assets/Scripts/ThirdPersonStatus.js
@@ -79,7 +79,7 @@ function Die ()
 	
 	// If we've reached here, the player still has lives remaining, so respawn.
 	respawnPosition = Respawn.currentRespawn.transform.position;
-	Camera.main.transform.position = respawnPosition - (transform.forward * 4) + Vector3.up;	// reset camera too
+	//Camera.main.transform.position = respawnPosition - (transform.forward * 4) + Vector3.up;	// reset camera too
 	// Hide the player briefly to give the death sound time to finish...
 	//SendMessage("HidePlayer");
 	
